e9f07379ebe5078ae2d70499bd5326c36682d7a4
Ebara Kazuhiro <romsound040220@gmail.com>
2019-02-19 17:15:19 +0900

    Add infinite loading repository

---
+++ infinite_loading/pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="container">
+    <div>
+      <InfiniteScroll />
+    </div>
+  </section>
+</template>
+
+<script>
+import InfiniteScroll from '~/components/InfiniteScroll.vue'
+
+export default {
+  components: {
+    InfiniteScroll
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
