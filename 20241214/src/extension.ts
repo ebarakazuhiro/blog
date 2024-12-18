acc1c6711935fa229858a67c81d1e7e96382ce8e
Kazuhiro Ebara <ebarakazuhiro@gmail.com>
2024-12-18 11:55:30 +0900

    Update extension file

--- 20241214/src/extension.ts
+++ 20241214/src/extension.ts
@@ -1,26 +1,32 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  context.subscriptions.push(
+    vscode.commands.registerCommand("catCoding.start", () => {
+      // Create and show panel
+      const panel = vscode.window.createWebviewPanel(
+        "catCoding",
+        "Cat Coding",
+        vscode.ViewColumn.One,
+        {}
+      );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "20241214" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('20241214.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from 20241214!');
-	});
-
-	context.subscriptions.push(disposable);
+      // And set its HTML content
+      panel.webview.html = getWebviewContent();
+    })
+  );
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+function getWebviewContent() {
+  return `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cat Coding</title>
+</head>
+<body>
+    <img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif" width="300" />
+</body>
+</html>`;
+}
